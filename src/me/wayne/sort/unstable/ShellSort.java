d5ed6f2f59a981c1aedccb8da8ccf91bcc3434e8
weimuhua <weimuhua0812@126.com>
2016-05-13 14:29:36 +0800

    change : add ShellSort.java.

---
+++ src/me/wayne/sort/unstable/ShellSort.java
@@ -0,0 +1,26 @@
+package me.wayne.sort.unstable;
+
+import java.util.List;
+
+import me.wayne.sort.SortUtils;
+
+public class ShellSort {
+
+    /**
+     * 希尔排序, 改进版的插入排序
+     * 时间复杂度,最坏O(n2),最好O(n*log2n)(n*(lgn平方))
+     * 思路:把待排序元素分为几个区域,这样可以让一个元素一次性朝最终位置前进一大步
+     * 然后算法取越来越小的部长,最后一步就是普通的插入排序了
+     */
+    public static void sort(List<Integer> data) {
+        int i, j, gap;
+        for (gap = data.size() / 2; gap > 0; gap /= 2) {
+            for (i = gap; i < data.size(); i++) {
+                for (j = i; j >= gap && data.get(j - gap) > data.get(j); j -= gap) {
+                    SortUtils.swap(data, j - gap, j);
+                }
+            }
+        }
+        System.out.println(data.toString());
+    }
+}
