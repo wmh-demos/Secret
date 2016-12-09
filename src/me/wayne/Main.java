5f1769b2923675a9126db7262b44abcd0790a551
weimuhua <weimuhua0812@126.com>
2016-12-09 17:24:41 +0800

    change : delete test BitTree in main method.

--- src/me/wayne/Main.java
+++ src/me/wayne/Main.java
@@ -1,7 +1,6 @@
 package me.wayne;
 
 
-import me.wayne.ds.TestBitTree;
 import me.wayne.sort.TestSort;
 
 
@@ -9,7 +8,5 @@ public class Main {
 
     public static void main(String[] args) {
         TestSort.sort();
-
-        TestBitTree.test();
     }
 }
