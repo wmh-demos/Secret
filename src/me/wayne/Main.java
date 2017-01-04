ba6ea269ac20f55e2ee447dfdb4169b83d9233b0
weimuhua0812 <weimuhua0812@126.com>
2017-01-04 20:39:13 +0800

    change : change main method.

--- src/me/wayne/Main.java
+++ src/me/wayne/Main.java
@@ -1,15 +1,11 @@
 package me.wayne;
 
 
-import me.wayne.ds.TestBitTree;
-import me.wayne.sort.TestSort;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        TestSort.sort();
 
-        TestBitTree.test();
     }
 }
