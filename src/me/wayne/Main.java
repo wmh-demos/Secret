18af280789b31a3f3c3d50bf32f3d58ef161376e
weimuhua <weimuhua0812@126.com>
2017-10-19 10:36:34 +0800

    修改Main.class方法

--- src/me/wayne/Main.java
+++ src/me/wayne/Main.java
@@ -1,10 +1,13 @@
 package me.wayne;
 
 
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, I'm Java code in Secret.jar .");
+    }
+
+    public static String getMessage() {
+        return "Hello, I'm Java code in Secret.jar .";
     }
 }
