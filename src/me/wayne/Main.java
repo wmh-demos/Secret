e1f3b9b3cad3473d5f29c83e7ae2682354182d69
weimuhua <weimuhua0812@126.com>
2017-11-23 18:24:49 +0800

    添加日志分析代码

--- src/me/wayne/Main.java
+++ src/me/wayne/Main.java
@@ -1,10 +1,14 @@
 package me.wayne;
 
 
+import me.wayne.file.LogCalculateUtil;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, I'm Java code in Secret.jar .");
+
+        LogCalculateUtil.calculate();
     }
 
     public static String getMessage() {
