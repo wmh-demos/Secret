2f7e9aa6fcdf7be17a3595257f7fb91e8955a918
weimuhua <weimuhua0812@126.com>
2019-11-24 13:40:43 +0800

    clean code.

--- src/main/java/me/wayne/ds/Traverse.java
+++ src/main/java/me/wayne/ds/Traverse.java
@@ -2,12 +2,12 @@ package me.wayne.ds;
 
 import java.util.Stack;
 
-public class Traverse {
+class Traverse {
 
-    public static void preOrder(BitTree tree) {
+    static void preOrder(BitTree tree) {
         if (tree == null) return;
 
-        System.out.printf(tree.value + " ");
+        System.out.print(tree.value + " ");
 
         if (tree.left != null) {
             preOrder(tree.left);
@@ -17,19 +17,19 @@ public class Traverse {
         }
     }
 
-    public static void inOrder(BitTree tree) {
+    static void inOrder(BitTree tree) {
         if (tree == null) return;
 
         if (tree.left != null) {
             inOrder(tree.left);
         }
-        System.out.printf(tree.value + " ");
+        System.out.print(tree.value + " ");
         if (tree.right != null) {
             inOrder(tree.right);
         }
     }
 
-    public static void postOrder(BitTree tree) {
+    static void postOrder(BitTree tree) {
         if (tree == null) return;
 
         if (tree.left != null) {
@@ -38,14 +38,14 @@ public class Traverse {
         if (tree.right != null) {
             postOrder(tree.right);
         }
-        System.out.printf(tree.value + " ");
+        System.out.print(tree.value + " ");
     }
 
-    public static void nonRecursionPreOrder(BitTree tree) {
-        Stack<BitTree> stack = new Stack<BitTree>();
+    static void nonRecursionPreOrder(BitTree tree) {
+        Stack<BitTree> stack = new Stack<>();
         while (!stack.isEmpty() || tree != null) {
             if (tree != null) {
-                System.out.printf(tree.value + " ");
+                System.out.print(tree.value + " ");
                 stack.push(tree);
                 tree = tree.left;
             } else {
@@ -55,15 +55,15 @@ public class Traverse {
         }
     }
 
-    public static void nonRecursionInOrder(BitTree tree) {
-        Stack<BitTree> stack = new Stack<BitTree>();
+    static void nonRecursionInOrder(BitTree tree) {
+        Stack<BitTree> stack = new Stack<>();
         while (!stack.isEmpty() || tree != null) {
             if (tree != null) {
                 stack.push(tree);
                 tree = tree.left;
             } else {
                 tree = stack.pop();
-                System.out.printf(tree.value + " ");
+                System.out.print(tree.value + " ");
                 tree = tree.right;
             }
         }
