a00c653e5718774e12c9f76aefa1cf83fe509dc7
weimuhua <weimuhua0812@126.com>
2020-05-12 23:32:26 +0800

    完成reverseHalf.

--- src/main/java/me/wayne/ds/Node.java
+++ src/main/java/me/wayne/ds/Node.java
@@ -14,14 +14,11 @@ public class Node {
         head.next = new Node(2);
         head.next.next = new Node(3);
         head.next.next.next = new Node(4);
-        Node reverse = Node.reverse(head);
-        while (reverse != null) {
-            System.out.println(reverse.value);
-            reverse = reverse.next;
-        }
+        Node.reverseHalf(head);
+        printNode(head);
     }
 
-    public static Node reverse(Node node) {
+    public static Node reverseRecursive(Node node) {
         if (node == null || node.next == null) {
             return node;
         }
@@ -37,5 +34,54 @@ public class Node {
         q.next = p;
 
         return reverseInner(q, temp);
+    }
+
+    private static Node reverseNonRecursive(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node pre = null;
+        Node next = null;
+        while (head != null) {
+            next = head.next;
+
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    private static void reverseHalf(Node head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+        //构造一个临时结点
+        Node pre = new Node(0);
+        pre.next = head;
+
+        Node half = head;
+        Node tail = head;
+        while (tail.next != null) {
+            //快慢指针，tail走两步，half走一步
+            tail = tail.next;
+            half = half.next;
+            pre = pre.next;
+
+            if (tail.next != null) {
+                tail = tail.next;
+            }
+        }
+
+        pre.next = null;
+        reverseNonRecursive(half);
+        pre.next = tail;
+    }
+
+    private static void printNode(Node node) {
+        while (node != null) {
+            System.out.println(node.value);
+            node = node.next;
+        }
     }
 }
