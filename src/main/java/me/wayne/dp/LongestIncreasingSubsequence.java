05daefba92a2638632a1d59ccd1ff9bf77890328
weimuhua <weimuhua0812@126.com>
2020-06-11 23:29:52 +0800

    feat: add LongestIncreasingSubsequence.java.

---
+++ src/main/java/me/wayne/dp/LongestIncreasingSubsequence.java
@@ -0,0 +1,34 @@
+package me.wayne.dp;
+
+import java.util.Arrays;
+
+/**
+ * 输入: [10,9,2,5,3,7,101,18]
+ * 输出: 4
+ * 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+ */
+class LongestIncreasingSubsequence {
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        //dp[i]的含义是以 num[i] 为结尾的最长递增子序列
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int value : dp) {
+            max = Math.max(max, value);
+        }
+        return max;
+    }
+}
