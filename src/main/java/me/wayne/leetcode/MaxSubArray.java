ff3e95c7e6e1e62a20a57f94dd43167e429f9a68
weimuhua <weimuhua0812@126.com>
2021-06-19 11:49:34 +0800

    feat: add MaxSubArray file
    https://leetcode-cn.com/problems/maximum-subarray/

---
+++ src/main/java/me/wayne/leetcode/MaxSubArray.java
@@ -0,0 +1,39 @@
+package me.wayne.leetcode;
+
+/**
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * <p>
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ * <p>
+ * https://leetcode-cn.com/problems/maximum-subarray/
+ */
+public class MaxSubArray {
+
+    /**
+     * 思考状态：dp[i]代表着以nums[i]结尾的最大的子序列和。
+     * 思考状态转移方程：dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+     * 取dp[i-1] + nums[i]和 nums[i] 的最大值是因为考虑 dp[i-1] 是否对 nums[i] 产生了负增益，
+     * 如果对nums[i]产生了负增益，那么不如不产生，对应的就是将dp[i-1]去掉。
+     * 思考初始化：dp[0] = nums[0]，所以i必须从1开始直到末尾。
+     * 思考输出：输出dp数组的最大值即可。
+     */
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = nums[i] + Math.max(dp[i - 1], 0);
+
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+        }
+        return max;
+    }
+}
