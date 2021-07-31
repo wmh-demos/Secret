646900ebdb5e5206f81a90b758772d9e6a1382f3
weimuhua <weimuhua0812@126.com>
2021-07-31 22:35:07 +0800

    feat: migrate to Kotlin DSL build scripts

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+    application
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "me.wayne"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.google.code.gson:gson:2.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+application {
+    // Define the main class for the application
+    mainClassName = "me.wayne.Main"
+}
+val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
