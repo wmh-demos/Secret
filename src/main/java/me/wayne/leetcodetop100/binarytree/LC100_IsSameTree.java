a3addbbf02a47b7c18a98f310bab424cc39e7c84
weimuhua <weimuhua0812@126.com>
2022-01-29 22:07:31 +0800

    feat: LC100_IsSameTree

---
+++ src/main/java/me/wayne/leetcodetop100/binarytree/LC100_IsSameTree.java
@@ -0,0 +1,23 @@
+package me.wayne.leetcodetop100.binarytree;
+
+import me.wayne.leetcodetop100.TreeNode;
+
+/**
+ * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+ * <p>
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ */
+public class LC100_IsSameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+
+        boolean sameVal = p.val == q.val;
+        return sameVal && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
