f85d09728a127e2488035beb8b32fa97921e29e1
weimuhua <weimuhua0812@126.com>
2022-02-08 17:36:55 +0800

    refactor: refactor LC300_LengthOfLIS

--- src/main/java/me/wayne/leetcodetop100/dp/LC300_LengthOfLIS.java
+++ src/main/java/me/wayne/leetcodetop100/dp/LC300_LengthOfLIS.java
@@ -8,6 +8,8 @@ import java.util.Arrays;
  * 输入: [10,9,2,5,3,7,101,18]
  * 输出: 4
  * 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+ * <p>
+ * https://leetcode-cn.com/problems/longest-increasing-subsequence/
  */
 class LC300_LengthOfLIS {
 
