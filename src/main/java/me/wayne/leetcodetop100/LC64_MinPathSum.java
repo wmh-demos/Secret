921295e16f01d8b35268bbf0074fd954db333a0f
weimuhua <weimuhua0812@126.com>
2022-03-12 14:37:32 +0800

    feat: add LC64_MinPathSum

---
+++ src/main/java/me/wayne/leetcodetop100/LC64_MinPathSum.java
@@ -0,0 +1,29 @@
+package me.wayne.leetcodetop100;
+
+/**
+ * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ * 说明：每次只能向下或者向右移动一步。
+ * <p>
+ * https://leetcode-cn.com/problems/minimum-path-sum/
+ */
+public class LC64_MinPathSum {
+
+    public int minPathSum(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] dp = new int[n][m];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
+        }
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                dp[i][j] = Math.min(grid[i][j] + dp[i - 1][j], grid[i][j] + dp[i][j - 1]);
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+}
