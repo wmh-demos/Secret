af0abb9d9b488496738aa0cbb24dedd4e8979f07
weimuhua <weimuhua0812@126.com>
2022-03-19 00:06:44 +0800

    feat: add LC198_Rob

---
+++ src/main/java/me/wayne/leetcodetop100/dp/LC198_Rob.java
@@ -0,0 +1,28 @@
+package me.wayne.leetcodetop100.dp;
+
+/**
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，
+ * 影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+ * 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * <p>
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/house-robber
+ */
+public class LC198_Rob {
+
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[n - 1];
+    }
+}
