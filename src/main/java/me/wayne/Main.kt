6593bf2137fb8598bfe19143378bf443a4fac56b
weimuhua <weimuhua0812@126.com>
2022-05-29 14:22:59 +0800

    feat: calculate total

--- src/main/java/me/wayne/Main.kt
+++ src/main/java/me/wayne/Main.kt
@@ -1,9 +1,25 @@
 package me.wayne
 
+import me.wayne.file.readFileAndReadAsLine
+
 object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
         println("Hello IntelliJ!!!")
+
+        calculateTotal()
+    }
+
+    private fun calculateTotal() {
+        var total = 0F
+        val lines = readFileAndReadAsLine("/Users/wayne/workspace/github/Secret/money.txt")
+        lines.forEach {
+            val index = it.indexOf("￥")
+            if (index >= 0 && index < it.length - 1) {
+                total += it.substring(index + 1).toFloat()
+            }
+        }
+        println("total: $total")
     }
 }
