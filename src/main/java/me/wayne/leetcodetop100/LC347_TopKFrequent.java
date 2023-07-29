1cd76bcee34accbca6d482d37455407a877bc831
weimuhua <weimuhua0812@126.com>
2023-07-29 12:26:08 +0800

    feat: add LC347_TopKFrequent.java

---
+++ src/main/java/me/wayne/leetcodetop100/LC347_TopKFrequent.java
@@ -0,0 +1,39 @@
+package me.wayne.leetcodetop100;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+ *
+ * <a href="https://leetcode.cn/problems/top-k-frequent-elements/">前 K 个高频元素</a>
+ */
+public class LC347_TopKFrequent {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+
+        // int[] 的第一个元素代表数组的值，第二个元素代表了该值出现的次数
+        PriorityQueue<int[]> queue = new PriorityQueue<>((m, n) -> m[1] - n[1]);
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        int[] ret = new int[k];
+        for (int i = 0; i < k; ++i) {
+            ret[i] = queue.poll()[0];
+        }
+        return ret;
+    }
+}
